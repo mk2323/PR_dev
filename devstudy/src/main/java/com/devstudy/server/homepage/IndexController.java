d8b8ac15f15b232d505e3221c715f5e1cdb9ea0a
9iseong <9iseong@gmail.com>
2020-04-09 17:11:09 +0900

    server basic setting

--- devstudy/src/main/java/com/devstudy/server/homepage/IndexController.java
+++ devstudy/src/main/java/com/devstudy/server/homepage/IndexController.java
@@ -1,28 +1,15 @@
-package com.zoomansa.homepage;
+package com.devstudy.server.homepage;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.zoomansa.homepage.area.service.ParkingAreaService;
-import com.zoomansa.homepage.media.service.MediaCenterService;
-
 @Controller
 public class IndexController {
 
-	@Autowired
-	ParkingAreaService parkingAreaService;
-	
-	@Autowired
-	MediaCenterService mediaCenterService;
-	
 	@RequestMapping(value="/")
-    public ModelAndView root() {
-		ModelAndView mav = new ModelAndView("/homepage/index");
-		mav.addObject("serviceArea", parkingAreaService.getServiceArea());
-		mav.addObject("mediaCenter", mediaCenterService.getUsedMediaList());
-		return mav;    
+    public String root() {
+		return "/index";    
     }
     
     @RequestMapping(value="/index")
